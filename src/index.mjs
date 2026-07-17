58384555ad8166a4ef00e7f6881e2e7bbb69438d
Alex Gusev <alex@flancer64.com>
2026-07-17 08:48:48 +0300

    feat: add src/index.mjs

---
+++ src/index.mjs
@@ -0,0 +1,2 @@
+export {createControlPlane, InMemoryPrincipalRepresentationReader, InMemoryInterpretationSessionStore, ScriptedModelClient} from "./control-plane.mjs";
+export {validateProposal, emptyProposal} from "./proposal.mjs";
